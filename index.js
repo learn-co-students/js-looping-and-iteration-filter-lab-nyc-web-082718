9c1d70250175d464ee27773f7469152bee3d2449
yammik <mkim0514@gmail.com>
2018-10-07 12:27:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+const findMatching = function(obj, key) {
+  return obj.filter(name => name.toLowerCase() === key.toLowerCase());
+}
+
+const fuzzyMatch = function(obj, key) {
+  return obj.filter(name => name.startsWith(key));
+}
+
+const matchName = function(obj, key) {
+  return obj.filter(entry => entry.name === key);
+}
